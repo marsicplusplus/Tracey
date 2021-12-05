90262596cfc7c8e971de34960f0d7ae698a020e4
Lorenzo Marsicano <lorenzomarsicano@gmail.com>
2021-12-05 22:54:37 +0100

    No idea, stuff is wrong

--- src/hittables/mesh.cpp
+++ src/hittables/mesh.cpp
@@ -40,16 +40,26 @@ bool Mesh::intersectTri(const Triangle &tri, const Ray &ray, HitRecord &rec) con
 		rec.t = tmp;
 		rec.p = ray.at(tmp);
 
-		const glm::dvec3 &n0 = this->norm[tri.normal.x]; 
-		const glm::dvec3 &n1 = this->norm[tri.normal.y]; 
-		const glm::dvec3 &n2 = this->norm[tri.normal.z]; 
-		glm::dvec3 hitNormal = (1.0 - u - v) * n0 + u * n1 + v * n2; 
-		rec.setFaceNormal(ray, hitNormal);
+		glm::dvec3 hitNormal;
+		if(tri.normal.x == -1 || tri.normal.y == -1 || tri.normal.z == -1)
+			hitNormal = glm::cross((v1 - v0), (v2 - v0));
+		else{
+			const glm::dvec3 &n0 = this->norm[tri.normal.x];
+			const glm::dvec3 &n1 = this->norm[tri.normal.y];
+			const glm::dvec3 &n2 = this->norm[tri.normal.z];
+			hitNormal = u * n1 + v * n2 + (1.0 - u - v) * n0; 
+		}
+		rec.setFaceNormal(ray, (hitNormal));
 
-		const glm::dvec2 &st0 = this->uvs[tri.texture.x]; 
-		const glm::dvec2 &st1 = this->uvs[tri.texture.y];
-		const glm::dvec2 &st2 = this->uvs[tri.texture.z]; 
-		glm::dvec2 uv = (1.0 - u - v) * st0 + u * st1 + v * st2; 
+		glm::dvec2 uv;
+		if(tri.texture.x == -1 || tri.texture.y == -1 || tri.texture.z == -1){
+			uv = glm::dvec2{0,0};
+		} else { 
+			const glm::dvec2 &st0 = this->uvs[tri.texture.x];
+			const glm::dvec2 &st1 = this->uvs[tri.texture.y];
+			const glm::dvec2 &st2 = this->uvs[tri.texture.z];
+			uv = u * st0 + v * st1 + (1.0 - u - v) * st2; 
+		}
 		rec.u = uv.x;
 		rec.v = uv.y;
 		rec.material = tri.mat;
