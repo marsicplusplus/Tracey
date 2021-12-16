142a0b8b3f587fc9a9b6a21b8ca71935e40b0106
Lorenzo Marsicano <lorenzomarsicano@gmail.com>
2021-12-16 16:44:10 +0100

    Stop normalizing direction of transformed ray

--- src/hittables/sphere.hpp
+++ src/hittables/sphere.hpp
@@ -12,10 +12,6 @@ class Sphere : public Hittable {
 		inline bool hit(const Ray &ray, float tMin, float tMax, HitRecord &rec) const override {
 			const auto transformedRay = ray.transformRay(transformInv);
 
-			if (rec.p != glm::fvec3{INF, INF, INF}) {
-				tMax = glm::distance(transformedRay.getOrigin(), glm::fvec3(transformInv * glm::fvec4(rec.p, 1.0f)));
-			}
-
 			if (!hitAABB(transformedRay)) {
 				return false;
 			}
@@ -38,9 +34,9 @@ class Sphere : public Hittable {
 				else if(t1 > 0) cT = t1;
 				else return false;
 				if(cT < tMax && cT > tMin){
+					rec.t = cT;
 					auto localp = transformedRay.at(cT);
 					rec.p = transform * glm::vec4(localp, 1.0f);
-					rec.t = glm::distance(rec.p, ray.getOrigin());
 					rec.material = mat;
 					glm::fvec3 normal = (localp) / radius;
 					auto transformedNormal = transposeInv * glm::dvec4(normal, 0);
